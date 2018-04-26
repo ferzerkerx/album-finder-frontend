c318fa349fe8f71c9fab798b16955fd5d34e9203
fernandoMontes <fernando@element.in>
2018-04-26 05:42:43 +0200

    - fixing tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,26 @@
-import { async, TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import {async, TestBed} from '@angular/core/testing';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from "./app-routing.module";
+import {AlbumsComponent} from "./albums/albums.component";
+import {ArtistsComponent} from "./artists/artists.component";
+import {FormsModule} from "@angular/forms";
+import {BrowserModule} from "@angular/platform-browser";
+import {InputComponent} from "./input/input.component";
+import {APP_BASE_HREF} from "@angular/common";
+import {BsModalService, ComponentLoaderFactory, PositioningService} from "ngx-bootstrap";
+import {UserService} from "./user.service";
+import {HttpClientModule} from "@angular/common/http";
 
 describe('AppComponent', () => {
   beforeEach(
     async(() => {
       TestBed.configureTestingModule({
-        declarations: [AppComponent]
+        imports:[BrowserModule,
+          FormsModule,
+          AppRoutingModule,HttpClientModule],
+        declarations: [AppComponent, AlbumsComponent, ArtistsComponent, InputComponent],
+        providers:[{provide: APP_BASE_HREF, useValue: '/'}, BsModalService, ComponentLoaderFactory,
+          PositioningService,UserService]
       }).compileComponents();
     })
   );
@@ -26,13 +41,13 @@ describe('AppComponent', () => {
     })
   );
   it(
-    'should render title in a h1 tag',
+    'should render a Login button',
     async(() => {
       const fixture = TestBed.createComponent(AppComponent);
       fixture.detectChanges();
       const compiled = fixture.debugElement.nativeElement;
-      expect(compiled.querySelector('h1').textContent).toContain(
-        'Welcome to app!'
+      expect(compiled.querySelector('.btn').textContent).toContain(
+        'Login'
       );
     })
   );
